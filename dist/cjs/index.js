97cbce832fd9cd8980657161b8472372d4f183b5
Luiz Imbroisi <luizimbroisi@gmail.com>
2025-02-23 08:02:48 -0300

    1.0.6 new

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -3,7 +3,7 @@
 var jsxRuntime = require('react/jsx-runtime');
 
 const HelloWorld = ({ name = 'World' }) => {
-    return jsxRuntime.jsxs("h1", { style: { whiteSpace: 'nowarp' }, children: ["Test 2 ", name] });
+    return jsxRuntime.jsxs("h1", { style: { whiteSpace: 'nowarp' }, children: ["Test 3 ", name] });
 };
 
 exports.HelloWorld = HelloWorld;
