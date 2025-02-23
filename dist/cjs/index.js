27bde09ca22e8937c4a84604c2a4a0716c3b365d
Luiz Imbroisi <luizimbroisi@gmail.com>
2025-02-23 07:14:40 -0300

    xy

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -3,7 +3,7 @@
 var jsxRuntime = require('react/jsx-runtime');
 
 const HelloWorld = ({ name = 'World' }) => {
-    return jsxRuntime.jsxs("h1", { children: ["Hello, ", name, "!"] });
+    return jsxRuntime.jsxs("h1", { children: ["==== Hello, ", name, "!"] });
 };
 
 exports.HelloWorld = HelloWorld;
