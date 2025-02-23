a370b0740b7536f652f486d02b7413ece2aeb517
Luiz Imbroisi <luizimbroisi@gmail.com>
2025-02-23 07:24:49 -0300

    1.0.5

--- dist/cjs/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var jsxRuntime = require('react/jsx-runtime');
-
-const HelloWorld = ({ name = 'World' }) => {
-    return jsxRuntime.jsxs("h1", { children: ["==== Hello, ", name, "!"] });
-};
-
-exports.HelloWorld = HelloWorld;
--- dist/esm/index.js
+++
@@ -1,7 +0,0 @@
-import { jsxs } from 'react/jsx-runtime';
-
-const HelloWorld = ({ name = 'World' }) => {
-    return jsxs("h1", { children: ["==== Hello, ", name, "!"] });
-};
-
-export { HelloWorld };
